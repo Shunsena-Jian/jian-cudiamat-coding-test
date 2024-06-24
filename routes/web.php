710635ab7b831bf5c020edbc972b42ad40c57d0f
Jian Raphael R. Cudiamat <jian.r.cudiamat@gmail.com>
2024-06-24 08:52:22 +0800

    UPDATE 6 * web.php // Added auth middleware for the routes

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/get_product', [ProductsController::class, 'retrieveOwnedProducts'])->name('get_product');
-Route::post('/add_product', [ProductsController::class, 'addProduct'])->name('add_product');
-Route::post('/edit_product', [ProductsController::class, 'editProduct'])->name('edit_product');
-Route::delete('/delete_product', [ProductsController::class, 'deleteProduct'])->name('delete_product');
-Route::get('/view_product_details', [ProductsController::class, 'viewProductDetails'])->name('view_product_details');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/get_product', [ProductsController::class, 'retrieveOwnedProducts'])->name('get_product');
+    Route::post('/add_product', [ProductsController::class, 'addProduct'])->name('add_product');
+    Route::post('/edit_product', [ProductsController::class, 'editProduct'])->name('edit_product');
+    Route::delete('/delete_product', [ProductsController::class, 'deleteProduct'])->name('delete_product');
+    Route::get('/view_product_details', [ProductsController::class, 'viewProductDetails'])->name('view_product_details');
+});
